334bda3fe5c864c77cb118c3adf36be90fb20912
Austin Leung <austinle@andrew.cmu.edu>
2022-03-20 14:35:52 -0400

    Add fe build files

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
     {
@@ -35,9 +36,6 @@ let notes = [
         important: true
     }
 ]
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
